41cdf1316683b960c707078310815dbf6c69ba08
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-01-30 20:48:38 -0300

    Alteração Necessária

--- src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -2,7 +2,8 @@ package org.generation.blogpessoal.repository;
 
 import org.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface PostagemRepository extends JpaRepository <Postagem,Long> {
 
 
